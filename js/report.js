07c681dab9bd8c4f060ce86a4545a1293f83a36c
La Schiocherie <aschiochet@mediametrie.fr>
2015-02-20 12:25:52 +0100

    fix icon header

--- js/report.js
+++ js/report.js
@@ -117,7 +117,7 @@ function showScenarios(node, data) {
 			th.append("span").attr("class", "glyphicon glyphicon-remove text-danger").attr("title", "% of failures");
 			break;
 		case "successes":
-			th.append("span").attr("class", "glyphicon glyphicon-remove text-success").attr("title", "% of successes");
+			th.append("span").attr("class", "glyphicon glyphicon-ok text-success").attr("title", "% of successes");
 			break;
 		default:
 			th.text(column);
